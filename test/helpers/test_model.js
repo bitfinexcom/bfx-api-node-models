37df3855afa3c1ea99bf6cc1693b7f094b6ca2ab
Cris Mihalache <me@f3rno.com>
2019-06-08 16:57:28 +0700

    (refactor) make model collections iterable

--- test/helpers/test_model.js
+++ test/helpers/test_model.js
@@ -52,6 +52,55 @@ const testModel = ({ values = {}, model, orderedFields, boolFields = [] }) => {
     checkModelFields(m)
   })
 
+  it('constructs from an array of arrays', () => {
+    const m = new Model([fieldValues, fieldValues, fieldValues])
+    const mArr = [...m]
+
+    // test [] access
+    for (let i = 0; i < m.length; i += 1) {
+      checkModelFields(m[i])
+    }
+
+    // test iterator
+    for (let i = 0; i < mArr.length; i += 1) {
+      checkModelFields(mArr[i])
+    }
+
+    assert.strictEqual(m.length, mArr.length)
+
+    // test equality
+    for (let i = 0; i < m.length; i += 1) {
+      assert.deepStrictEqual(m[i], mArr[i])
+    }
+  })
+
+  it('constructs from an array of objects', () => {
+    const data = {}
+    fields.forEach(f => (f !== null) && (data[f] = f))
+    boolFields.forEach((f) => { data[f] = false })
+    Object.assign(data, values)
+
+    const m = new Model([data, data, data])
+    const mArr = [...m]
+
+    // test [] access
+    for (let i = 0; i < m.length; i += 1) {
+      checkModelFields(m[i])
+    }
+
+    // test iterator
+    for (let i = 0; i < mArr.length; i += 1) {
+      checkModelFields(mArr[i])
+    }
+
+    assert.strictEqual(m.length, mArr.length)
+
+    // test equality
+    for (let i = 0; i < m.length; i += 1) {
+      assert.deepStrictEqual(m[i], mArr[i])
+    }
+  })
+
   it('serializes correctly', () => {
     const data = {}
     fields.forEach(f => (f !== null) && (data[f] = f))
