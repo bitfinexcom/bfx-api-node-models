a8c4f697e8f3294f87b794c46a7467e88b09e745
Cris Mihalache <me@f3rno.com>
2019-09-11 17:59:31 +0700

    (feature) add basic example

---
+++ examples/order.js
@@ -0,0 +1,16 @@
+'use strict'
+
+process.env.DEBUG = '*'
+
+const debug = require('debug')('bfx:api:models:examples:order')
+const { Order } = require('../')
+const o = new Order({
+  type: Order.type.EXCHANGE_LIMIT,
+  symbol: 'tBTCUSD',
+  amount: 0.05,
+  price: 10000,
+  hidden: true,
+})
+
+debug('generated ws2 compatible new order packet:')
+debug(JSON.stringify(o.toNewOrderPacket(), null, 2))
