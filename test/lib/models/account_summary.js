5152816ad7424cc9bfbfcce05658bae112ef21f1
ezeswci <wernicke.ezequiel@gmail.com>
2021-02-22 18:27:02 +1100

    Add new tests for account summary

--- test/lib/models/account_summary.js
+++ test/lib/models/account_summary.js
@@ -4,7 +4,29 @@
 const assert = require('assert')
 const { AccountSummary } = require('../../../lib')
 const testModelValidation = require('../../helpers/test_model_validation')
-
+const tradingVol30d = [
+  {
+    curr: 'BTC',
+    vol: 0.01917394
+  },
+  {
+    curr: 'ETH',
+    vol: 0.67504702
+  },
+  {
+    curr: 'XRP',
+    vol: 11243.7965533
+  },
+  {
+    curr: 'Total (USD)',
+    vol: 7621.42803742,
+    vol_safe: 7621.42803742,
+    vol_maker: 6673.18852682,
+    vol_BFX: 7621.42803742,
+    vol_BFX_safe: 7621.42803742,
+    vol_BFX_maker: 6673.18852682
+  }
+]
 const DATA = [
   null,
   null,
@@ -14,8 +36,16 @@ const DATA = [
     [0.001, 0.001, 0.001, null, null, -0.0002],
     [0.002, 0.0021, 0.0022, null, null, 0.00075]
   ],
-  null,
-  null,
+  [
+    tradingVol30d,
+    { USD: 8.56966754, BTC: 1.00006754 },
+    30008.56966754
+  ],
+  [
+    null,
+    { USD: 3.14966754, ETH: 0.10302856 },
+    283.14966754
+  ],
   null,
   null,
   { leo_lev: 0, leo_amount_avg: 0.002 }
@@ -25,6 +55,11 @@ describe('Symbol Details model', () => {
   testModelValidation({
     model: AccountSummary,
     validData: {
+      trade_vol_30d: new Array(...(new Array(5))).map(() => tradingVol30d),
+      fees_trading_30d: new Array(...(new Array(5))).map(() => {}),
+      fees_trading_total_30d: new Array(...(new Array(5))).map(() => Math.random()),
+      fees_funding_30d: new Array(...(new Array(5))).map(() => {}),
+      fees_funding_total_30d: new Array(...(new Array(5))).map(() => Math.random()),
       makerFee: new Array(...(new Array(5))).map(() => Math.random()),
       derivMakerRebate: new Array(...(new Array(5))).map(() => Math.random()),
       takerFeeToCrypto: new Array(...(new Array(5))).map(() => Math.random()),
@@ -38,6 +73,17 @@ describe('Symbol Details model', () => {
 
   it('initializes correctly', () => {
     const summary = new AccountSummary(DATA)
+    assert.deepStrictEqual(summary.trade_vol_30d, tradingVol30d)
+    assert.deepStrictEqual(
+      summary.fees_trading_30d,
+      { USD: 8.56966754, BTC: 1.00006754 }
+    )
+    assert.strictEqual(summary.fees_trading_total_30d, 30008.56966754)
+    assert.deepStrictEqual(
+      summary.fees_funding_30d,
+      { USD: 3.14966754, ETH: 0.10302856 }
+    )
+    assert.strictEqual(summary.fees_funding_total_30d, 283.14966754)
     assert.strictEqual(summary.makerFee, 0.001)
     assert.strictEqual(summary.derivMakerRebate, -0.0002)
     assert.strictEqual(summary.takerFeeToCrypto, 0.002)
@@ -57,6 +103,17 @@ describe('Symbol Details model', () => {
 
   it('unserializes correctly', () => {
     const summary = AccountSummary.unserialize(DATA)
+    assert.deepStrictEqual(summary.trade_vol_30d, tradingVol30d)
+    assert.deepStrictEqual(
+      summary.fees_trading_30d,
+      { USD: 8.56966754, BTC: 1.00006754 }
+    )
+    assert.strictEqual(summary.fees_trading_total_30d, 30008.56966754)
+    assert.deepStrictEqual(
+      summary.fees_funding_30d,
+      { USD: 3.14966754, ETH: 0.10302856 }
+    )
+    assert.strictEqual(summary.fees_funding_total_30d, 283.14966754)
     assert.strictEqual(summary.makerFee, 0.001)
     assert.strictEqual(summary.derivMakerRebate, -0.0002)
     assert.strictEqual(summary.takerFeeToCrypto, 0.002)
